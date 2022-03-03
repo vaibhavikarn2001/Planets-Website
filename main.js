c5154eca41873f4d44d41b1740c895971687b0e7
Vaibhavi Karn <76275888+vaibhavikarn2001@users.noreply.github.com>
2022-03-03 14:49:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+// LANDING PAGE ANIMATION
+var scene = document.getElementById("scene");
+var parallaxInstance = new Parallax(scene);
+
+var keys = [
+  "Mercury",
+  "Venus",
+  "Earth",
+  "Mars",
+  "Jupiter",
+  "Saturn",
+  "Uranus",
+  "Neptune"
+];
+var slider = new Swiper(".swiper-container", {
+  
+  slidesPerView: "auto",
+  spaceBetween: 150,
+  centeredSlides: true,
+  mousewheel: true,
+  pagination: {
+    el: ".planet-links",
+    clickable: true,
+    renderBullet: function(index, className) {
+      return '<div class="' + className + '">' + keys[index] + "</div>";
+    }
+  }
+});
+
